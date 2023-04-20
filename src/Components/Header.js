3fd2514482a98178cd293b5f02a60e140092ac2d
BastingSumo <69335910+MarcusBloomfield@users.noreply.github.com>
2023-04-20 14:03:13 +0800

    Tau effyied

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
         <div className="header">
-            <h1> Warhammer </h1>
+            <h1> WARHAMMER </h1>
             <div className="headerAbout">
-                Details of my warhammer army for personal use.  
+                Details of my Tau Warhammer army for personal use.  
             </div>
         </div>
     );
